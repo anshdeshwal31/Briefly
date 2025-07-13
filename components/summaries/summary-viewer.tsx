1954f40afe9c90d83885165882d5b3c146d4e36a
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-07-14 04:25:48 +0530

    commit before making styling the main summary card

---
+++ components/summaries/summary-viewer.tsx
@@ -0,0 +1,6 @@
+
+export const SummaryViewer = ({summary}:{summary:string}) => {
+  return (
+    <div>{summary}</div>
+  )
+}
--- lib/summaries.ts
+++ lib/summaries.ts
@@ -5,3 +5,23 @@ export const getSummaries = async (userId:string|undefined) => {
     const summaries = sql`SELECT * FROM pdf_summaries WHERE user_id=${userId} ORDER BY created_at DESC`
     return summaries
 }
+
+export const getSummaryById = async (id:string) => {
+    try {
+        console.log("inside getSummaryById")
+        const sql = await getDbConnection();
+        const [summary] = await sql`SELECT id, user_id, title,      original_file_url,
+                            summary_text, created_at, updated_at,
+                            status, file_name, LENGTH(summary_text) - LENGTH(REPLACE
+                            (summary_text, ' ', '')) + 1 as word_count from
+                            pdf_summaries where id = ${id} ORDER BY
+                            created_at DESC;`
+        console.log("summary inside getSummaryById")
+        console.log({summary})
+        return summary;
+        
+    } catch (error) {
+        console.log("error while getting the summary: ",error);
+        return null
+    }
+}
